c75d0e95c0937e721cc5c5e2f19b563548d002d1
Indrajeet <2003indrajeet@gmail.com>
2023-05-25 22:31:36 +0530

    some more improvements

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -1,11 +1,11 @@
 const Training = require("../models/TrainingModel");
-const training=require("../sampledatabase/trainingdb.jsx")
+const training = require("../sampledatabase/trainingdb.jsx")
 const fs = require('fs');
 const path = require('path');
 
 module.exports.getAllTrainings = async (req, res, next) => {
   try {
-  const trainings = await Training.find();
+    const trainings = await Training.find();
     return res.json(trainings);
   } catch (ex) {
     next(ex);
@@ -13,10 +13,10 @@ module.exports.getAllTrainings = async (req, res, next) => {
 };
 module.exports.createTraining = async (req, res, next) => {
   try {
-const { trainingName, category, subcategory,message,price } = req.body;
-   const imageurl=req.file.path;
-const image='http://localhost:5000/'+imageurl;
-       const training = await Training.create({
+    const { trainingName, category, subcategory, message, price } = req.body;
+    const imageurl = req.file.path;
+    const image = process.env.HOST_URL + imageurl;
+    const training = await Training.create({
       trainingName,
       category,
       subcategory,
@@ -24,7 +24,7 @@ const image='http://localhost:5000/'+imageurl;
       price,
       image
     });
-    return res.json({ msg: "Training created", status: true ,training});
+    return res.json({ msg: "Training created", status: true, training });
   } catch (ex) {
     next(ex);
   }
@@ -33,18 +33,18 @@ const image='http://localhost:5000/'+imageurl;
 
 module.exports.deleteTraining = async (req, res, next) => {
   try {
-const { id } = req.body;
+    const { id } = req.body;
 
-  const training =await Training.findByIdAndDelete(id);
-  if (!training) {
-     return res.json({ msg: "Training not found", status: false });
+    const training = await Training.findByIdAndDelete(id);
+    if (!training) {
+      return res.json({ msg: "Training not found", status: false });
     }
-      const imageUrl = training.image;
+    const imageUrl = training.image;
     const imagePath = new URL(imageUrl).pathname;
 
     // Delete the associated image file
     const imagePathToDelete = path.join(__dirname, '..', imagePath);
-   
+
     fs.unlink(imagePathToDelete, (err) => {
       if (err) {
         console.error(err);
@@ -59,7 +59,7 @@ const { id } = req.body;
 
 module.exports.editTraining = async (req, res, next) => {
   try {
- const { id, trainingName, price, category, subcategory } = req.body;
+    const { id, trainingName, price, category, subcategory } = req.body;
 
     // Check if the provided ID is valid
     if (!id) {
@@ -71,16 +71,16 @@ module.exports.editTraining = async (req, res, next) => {
 
     // Check if the training exists
     if (!training) {
-          return res.json({ msg: "Training not found", status: false });
+      return res.json({ msg: "Training not found", status: false });
     }
-       // Update the training properties with the new values
+    // Update the training properties with the new values
     training.trainingName = trainingName || training.trainingName;
     training.price = price || training.price;
     training.category = category || training.category;
     training.subcategory = subcategory || training.subcategory;
 
 
-  // Save the updated training
+    // Save the updated training
     await training.save();
     return res.json({ msg: "Training updated", status: true });
   } catch (ex) {
@@ -92,13 +92,13 @@ module.exports.editTraining = async (req, res, next) => {
 
 
 
- //inserting sample trainings  
- module.exports.insertSampleTrainingData = async (req, res,next) => {
+//inserting sample trainings  
+module.exports.insertSampleTrainingData = async (req, res, next) => {
   try {
     await Training.insertMany(training);
     res.status(200).json({ message: "Training data inserted successfully" });
   } catch (ex) {
-   next(ex);
-  } 
-};  
+    next(ex);
+  }
+};
 
